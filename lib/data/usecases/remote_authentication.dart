256e372b693eb680a92734859fa5223122681eea
Paulo jorge <paulosoujava@gmail.com>
2020-11-27 13:21:34 -0300

    return an Account if HttpClient returns 200

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -2,7 +2,8 @@
 import 'package:meta/meta.dart';
 //domain
 import '../../domain/usecases/authentication.dart';
-import 'package:clean_code/domain/helpers/helpers.dart';
+import '../../domain/helpers/helpers.dart';
+import '../../domain/entities/entities.dart';
 //data
 import '../http/http.dart';
 
@@ -11,12 +12,17 @@ class RemoteAuthentication {
   final String url;
 
   RemoteAuthentication({@required this.httpClient, @required this.url});
-  Future<void> auth(AuthenticationParams params) async {
+  Future<AccountEntity> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     try {
-      await httpClient.request(url: url, method: 'post', body: body);
-    } on HttpError catch(error){
-      throw error == HttpError.unauthorized ? DomainError.invalidCredentials : DomainError.unexpected;
+      final httpResponse =
+          await httpClient.request(url: url, method: 'post', body: body);
+      return AccountEntity.fromJson(httpResponse);
+
+    } on HttpError catch (error) {
+      throw error == HttpError.unauthorized
+          ? DomainError.invalidCredentials
+          : DomainError.unexpected;
     }
   }
 }
