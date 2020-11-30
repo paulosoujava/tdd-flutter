2aed39160013689693dedb72ffc1613d36ed6f5c
Paulo Oliveira <paulosoujava@gmail.com>
2020-11-30 08:13:08 -0300

    refactor past

--- test/presentation/presenter/stream_login_presenter_test.dart
+++ test/presentation/presenter/stream_login_presenter_test.dart
@@ -1,33 +1,9 @@
-import 'dart:async';
-
 import 'package:faker/faker.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-abstract class Validation {
-  String validate({@required String field, @required String value});
-}
-
-class LoginState {
-  String emailError;
-}
-
-class StreamLoginPresenter {
-  final Validation validation;
-  final _controller = StreamController<LoginState>();
-
-  var _state = LoginState();
-
-  Stream<String> get emailErrorStream =>
-      _controller.stream.map((state) => state.emailError);
-  StreamLoginPresenter({@required this.validation});
-
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-}
+import 'package:clean_code/presentation/presenters/stream_login_presenter.dart';
+import 'package:clean_code/presentation/protocols/validation.dart';
 
 class ValidationSpy extends Mock implements Validation {}
 
