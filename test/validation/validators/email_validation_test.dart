bf2e15f967940581c36a2caf75f7da4d386ad736
Paulo Oliveira <paulosoujava@gmail.com>
2020-11-30 13:59:18 -0300

    move regex

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,17 +1,6 @@
-import 'package:clean_code/validation/protocols/field_validation.dart';
 import 'package:test/test.dart';
 
-class EmailValidation implements FieldValidation {
-  final String field;
-
-  EmailValidation(this.field);
-
-  String validate(String value) {
-    final regex = RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
-    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
-    return isValid ? null : 'Campo inválido';
-  }
-}
+import 'package:clean_code/validation/validators/validators.dart';
 
 void main() {
   EmailValidation sut;
